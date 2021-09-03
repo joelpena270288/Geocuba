dd49ed352e0c22ba71731d62e150581a0f7dfb90
Joel Peña González <joelpenagonzalez122@gmail.com>
2021-09-03 00:45:58 -0400

    first commit

--- src/modules/planificador/planificador.service.ts
+++ src/modules/planificador/planificador.service.ts
@@ -9,27 +9,21 @@ import {
 } from '@nestjs/common';
 @Injectable()
 export class PlanificadorService {
-  async getPlanPrueba(): Promise<string[]> {
-    let result:string[] = [];
-    try {
-      await exec(
-        'cd /home/ubuntu/Geocuba/src/optic/ && ./optic-clp domain.pddl prueba.pddl',
-        (error, stdout, stderr) => {
-          if (error) {
-            console.log(`error: ${error.message}`);
+  async getPlanPrueba(): Promise<any> {
+
+    const cmder = async cmd => {
+      return new Promise((resolve, reject) => {
+        exec('cd /home/ubuntu/Geocuba/src/optic/ && ./optic-clp domain.pddl prueba.pddl', (err, stdout, stderr) => {
+          if(err) {
+            reject(err);
             return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-        },
-      );
-    } catch (e) {
-      throw new BadRequestException(e);
+          }
+          resolve(stdout);
+        })
+      })
     }
-    return result;
+   return cmder;
+   
   }
   async picarSalida(stdout): Promise<string[]> {
     const salidacompleta: string[] = stdout.split('Cost:');
