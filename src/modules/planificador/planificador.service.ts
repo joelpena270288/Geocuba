9390586f88d4f539cda1d14e02d11b3c1f6e5535
Joel Peña González <joelpenagonzalez122@gmail.com>
2021-09-02 23:40:49 -0400

    first commit

--- src/modules/planificador/planificador.service.ts
+++ src/modules/planificador/planificador.service.ts
@@ -9,15 +9,14 @@ import {
 } from '@nestjs/common';
 @Injectable()
 export class PlanificadorService {
-  async getPlanPrueba():Promise<string> {
-    let result = "";
-    await exec(
-     "cd /home/ubuntu/Geocuba/src/optic/ && ./optic-clp domain.pddl prueba.pddl",
+  async getPlanPrueba(): Promise<any> {
+    const result = await exec(
+      'cd /home/ubuntu/Geocuba/src/optic/ && ./optic-clp domain.pddl prueba.pddl',
       async function (error, stdout, stderr) {
         if (error) {
           throw new BadRequestException(error);
         } else {
-          result = await stdout;
+          return stdout;
         }
       },
     );
