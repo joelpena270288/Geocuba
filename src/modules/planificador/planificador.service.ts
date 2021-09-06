e1c648b884616411dac62ab3b4fd3d53c73bda09
Joel Peña González <joelpenagonzalez122@gmail.com>
2021-09-05 22:10:51 -0400

    first commit

--- src/modules/planificador/planificador.service.ts
+++ src/modules/planificador/planificador.service.ts
@@ -70,7 +70,7 @@ export class PlanificadorService {
     });
   }
   borrarDatos(cmd) {
-    await exec(cmd, async function (error: any, stdout: string, stderr: any) {
+   exec(cmd, async function (error: any, stdout: string, stderr: any) {
       if (error) {
         console.log('ERROR!!!!!!', error);
       }
